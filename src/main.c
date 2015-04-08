01395d1cc1cd833b47dfebcd2c5cb11baa523ad1
Steffan 'Ruirize' James <ruirize@ruirize.co.uk>
2015-04-08 16:19:56 +0100

    Included all relevant images.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,19 @@ static Window *window_main;
 static BitmapLayer *layer_background;
 
 static GBitmap *bitmap_background;
+static GBitmap *bitmap_tile;
+static GBitmap *bitmap_pipe_connector_black;
+static GBitmap *bitmap_pipe_connector_white;
+static GBitmap *bitmap_pipe_elbow_black;
+static GBitmap *bitmap_pipe_elbow_white;
+static GBitmap *bitmap_pipe_horizontal_black;
+static GBitmap *bitmap_pipe_horizontal_white;
+static GBitmap *bitmap_pipe_plus_black;
+static GBitmap *bitmap_pipe_plus_white;
+static GBitmap *bitmap_pipe_tee_black;
+static GBitmap *bitmap_pipe_tee_white;
+static GBitmap *bitmap_pipe_vertical_black;
+static GBitmap *bitmap_pipe_vertical_white;
 
 static void window_main_load() {
   
@@ -24,6 +37,19 @@ static void init() {
   window_stack_push(window_main, true);
   
   bitmap_background = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  bitmap_tile = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_TILE);
+  bitmap_pipe_connector_black = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_CONNECTOR_BLACK);
+  bitmap_pipe_connector_white = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_CONNECTOR_WHITE);
+  bitmap_pipe_elbow_black = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_ELBOW_BLACK);
+  bitmap_pipe_elbow_white = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_ELBOW_WHITE);
+  bitmap_pipe_horizontal_black = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_HORIZONTAL_BLACK);
+  bitmap_pipe_horizontal_white = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_HORIZONTAL_WHITE);
+  bitmap_pipe_plus_black = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_PLUS_BLACK);
+  bitmap_pipe_plus_white = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_PLUS_WHITE);
+  bitmap_pipe_tee_black = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_TEE_BLACK);
+  bitmap_pipe_tee_white = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_TEE_WHITE);
+  bitmap_pipe_vertical_black = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_VERTICAL_BLACK);
+  bitmap_pipe_vertical_white = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_PIPE_VERTICAL_WHITE);
   
   GColor8 *palette_background = gbitmap_get_palette(bitmap_background);
   palette_background[1] = (GColor8){.argb = 0b11001100};
@@ -32,12 +58,26 @@ static void init() {
   
   layer_background = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(layer_background, bitmap_background);
-  
+
   layer_add_child(window_get_root_layer(window_main), bitmap_layer_get_layer(layer_background));
 }
 static void deinit() {
   window_destroy(window_main);
   
+  gbitmap_destroy(bitmap_background);
+  gbitmap_destroy(bitmap_tile);
+  gbitmap_destroy(bitmap_pipe_connector_black);
+  gbitmap_destroy(bitmap_pipe_connector_white);
+  gbitmap_destroy(bitmap_pipe_elbow_black);
+  gbitmap_destroy(bitmap_pipe_elbow_white);
+  gbitmap_destroy(bitmap_pipe_horizontal_black);
+  gbitmap_destroy(bitmap_pipe_horizontal_white);
+  gbitmap_destroy(bitmap_pipe_plus_black);
+  gbitmap_destroy(bitmap_pipe_plus_white);
+  gbitmap_destroy(bitmap_pipe_tee_black);
+  gbitmap_destroy(bitmap_pipe_tee_white);
+  gbitmap_destroy(bitmap_pipe_vertical_black);
+  gbitmap_destroy(bitmap_pipe_vertical_white);
 }
 
 int main() {
